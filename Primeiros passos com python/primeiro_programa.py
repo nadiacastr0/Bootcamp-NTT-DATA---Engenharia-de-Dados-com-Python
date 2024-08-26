da2ac4bb151738776c30336047c70f1a5bb82b04
nadiacastr0 <nadianaely32@gmail.com>
2024-08-26 15:10:03 -0300

    add new files

---
+++ Primeiros passos com python/primeiro_programa.py
@@ -0,0 +1,14 @@
+# print("Hello world!")
+
+#### Testando classe String ####
+
+nome = "Python"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+print(nome.strip())
+print(nome.lstrip())
+print(nome.rstrip())
+print(nome.center(10, "-"))
+print(" ".join(nome))
